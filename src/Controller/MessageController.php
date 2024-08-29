e1f5c84873a0f006ab336d87a3f39dfaf4248bbc
waldi <wfubu@web.de>
2024-08-29 23:25:12 +0200

    Mercure stuff changed

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -7,23 +7,19 @@ use App\Entity\Message;
 use App\Entity\User;
 use App\Form\MessageType;
 use App\Repository\MessageRepository;
-use App\Repository\UserRepository;
-use App\Service\CookieGenerator;
 use Doctrine\ORM\EntityManagerInterface;
-use JsonException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\Mercure\Update;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Uid\Uuid;
-
 
 #[Route(path: '/messages', name: 'messages_')]
 class MessageController extends AbstractController
@@ -34,14 +30,12 @@ class MessageController extends AbstractController
     ) {
     }
 
-    //BREAD controller action pattern
+    // BREAD controller action pattern
 
     /**
-     * Display list of messages from conversation
+     * Display list of messages from conversation.
      *
-     * @param GroupConversation $groupConversation
      * @param User|null $user
-     * @return Response
      */
     #[Route(path: '/{groupConversation}', name: 'browse')]
     public function browse(
@@ -56,10 +50,10 @@ class MessageController extends AbstractController
          *  $participantIds = array_map(fn (User $user) => $user->getId(), $group->getParticipants()->getValues();
          */
 
-
         /** @var Message $m */
-        $messages = array_map(function ($m) use ($user) {
+        $messages = array_map(static function ($m) use ($user) {
             $m->setMine($m->getUser() === $user);
+
             return $m;
         }, $messages);
 
@@ -69,23 +63,23 @@ class MessageController extends AbstractController
         ]);
     }
 
-
     /**
      * @throws \JsonException
      */
     #[Route(path: '/{id}/add', name: 'add', requirements: ['id' => '\d+'])]
-    #[IsGranted("ROLE_USER")]
+    #[IsGranted('ROLE_USER')]
     public function add(
         Request $request,
         HubInterface $hub,
         GroupConversation $groupConversation,
         #[CurrentUser] ?User $user
     ): Response {
-//        $this->denyAccessUnlessGranted('ROLE_USER');
-        //used with connected user
-        //$user = $this->security->getUser();
+        //        $this->denyAccessUnlessGranted('ROLE_USER');
+        // used with connected user
+        // $user = $this->security->getUser();
         if (!$user instanceof UserInterface) {
-            $this->addFlash('error', 'Utilisateur incorrect.');
+            $this->addFlash('error', 'User incorrect.');
+
             return $this->redirectToRoute('app_login');
         }
 
@@ -99,39 +93,47 @@ class MessageController extends AbstractController
             $message->setCreated(new \DateTime('now'));
             $message->setUpdated(new \DateTime('now'));
             $message->setContent($content);
-//            $message->setMine(true);
+            //            $message->setMine(true);
             $message->setSeen(false);
 
             $message->setUser($user);
             $groupConversation->addMessage($message);
 
+            //            dd($groupConversation->getUsers());
+
             try {
                 $date = new \DateTime('now');
                 $update = new Update(
-                    '/messages/' . $groupConversation->getId(),
-                    //IRI, the topic being updated, can be any string usually URL
+                    '/messages/'.$groupConversation->getId(),
+                    // IRI, the topic being updated, can be any string usually URL
                     json_encode([
-                        'conversation' => 'Nouveau message conversation :' . $groupConversation->getName(),
+                        'conversation' => 'Nouveau message conversation :'.$groupConversation->getName(),
                         'message' => $content,
                         'from' => $user->getUsername(),
                         'to' => $groupConversation->getUsers(),
                         'date' => $date->format('H:i'),
                     ], JSON_THROW_ON_ERROR),
-                    //the content of the update, can be anything
+                    // the content of the update, can be anything
                     $groupConversation->getPrivate(),
-                    //private
-                    'message-' . Uuid::v4(),
-                    //mercure id
+                    // private
+                    'message-'.Uuid::v4(),
+                    // mercure id
                     'message'
                 );
 
-//                dd($update);
-//                var_dump($update);
+                // PUBLISHER JWT : doit contenir la liste des conversations dans lesquels il peut publier conf => mercure.publish
+                // SUBSCRIBER JWT: doit contenir la liste des conversations dans lesquels il peut recevoir conf => mercure.subcribe
+
+                // Publisher's JWT must contain this topic, a URI template it matches or * in mercure.publish or you'll get a 401
+                // Subscriber's JWT must contain this topic, a URI template it matches or * in mercure.subscribe to receive the update
+
+                //                dd($update);
+                //                var_dump($update);
 
                 $hub->publish($update);
                 $this->em->flush();
             } catch (\Exception $e) {
-                //dd($groupConversation);
+                // dd($groupConversation);
                 throw $e;
             }
         }
@@ -140,26 +142,27 @@ class MessageController extends AbstractController
     }
 
     /**
-     * Ping mercure
+     * Ping mercure.
+     *
      * @throws \JsonException
      */
     #[Route(path: '/{id}/ping', name: 'ping')]
-    public function ping(Request $request, HubInterface $hub, int $id = null): RedirectResponse
+    public function ping(Request $request, HubInterface $hub, ?int $id = null): RedirectResponse
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
-//        dd($id);
+        //        dd($id);
 
         $update = new Update(
-            '/ping/' . $request->get('id'), //IRI, the topic being updated, can be any string usually URL
-            json_encode(['message' => 'pinged !'], JSON_THROW_ON_ERROR), //the content of the update, can be anything
-            false, //private
-            'ping-' . Uuid::v4(), //mercure id
+            '/ping/'.$request->get('id'), // IRI, the topic being updated, can be any string usually URL
+            json_encode(['message' => 'pinged !'], JSON_THROW_ON_ERROR), // the content of the update, can be anything
+            false, // private
+            'ping-'.Uuid::v4(), // mercure id
             'ping'
         );
 
         $hub->publish($update);
-//        dd($update);
-//        dd($hub);
+        //        dd($update);
+        //        dd($hub);
 
         return $this->redirectToRoute('messages_browse', ['groupConversation' => $request->get('id')]);
     }

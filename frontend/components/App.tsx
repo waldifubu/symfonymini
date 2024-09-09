625c95d5deb16fbff85dfefd946181f3925b7777
waldi <wfubu@web.de>
2024-09-10 00:18:28 +0200

    Reactstrap used

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,6 +1,7 @@
 import './app.css';
 import React from 'react';
 import {Overview} from "./Overview";
+import {Alert} from "reactstrap";
 //import * as bootstrap from 'bootstrap';
 // import { Container, Collapse, Popover, Toast, Tooltip, Alert, Modal, Dropdown } from bootstrap;
 
@@ -8,7 +9,8 @@ export const App = () => {
     return (
         <div className="App">
             <header className="App-header container">
-               <h2>Podcast Management</h2>
+                <Alert className="mt-2" color="light"><h2>Podcast Management</h2></Alert>
+
 
                 <Overview/>
             </header>
